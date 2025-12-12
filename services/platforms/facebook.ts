37190e3f44709c6e2b4f501f05ab3c8a086d69ab
menturkmen222-debug <menturkmen222@gmail.com>
2025-12-12 11:02:18 +0500

    Update facebook.ts

--- services/platforms/facebook.ts
+++ services/platforms/facebook.ts
@@ -1,6 +1,6 @@
 import type { Env } from "../../index.ts";
 import { Logger } from "../../utils/logger.ts";
-
+ 
 export async function uploadToFacebook(env: Env, video: any): Promise<boolean> {
   const logger = new Logger(env);
   await logger.info("⚠️ Facebook upload stub ishladi", { videoId: video.id });
